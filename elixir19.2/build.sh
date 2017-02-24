107b191fca94dee73420c1e8b30f397e98ba7694
Keith Brings <keith.brings@noizu.com>
2017-02-25 00:42:23 +0700

    container name fix

--- elixir19.2/build.sh
+++ elixir19.2/build.sh
@@ -2,4 +2,4 @@
 # Copyright © 2017 Noizu Labs, Inc. All rights reserved.
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-sudo docker build -t "registry.noizu.com/lacrosse.elixir19.2" $DIR
+sudo docker build -t "registry.noizu.com/noizu.elixir19.2" $DIR
